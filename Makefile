951b8e076bad49a29081da255455c527bc1081db
Julien Girardin <julien.girardin@scality.com>
2018-10-13 18:34:08 +0200

    Release process to buildbot-kubernetes.github.com

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
 
+V=@
+
 all:
 	skaffold run
+
+
+release:
+
+	$(V)if ./hack/release.sh --check-release; then \
+	   ./hack/release.sh --release; \
+	fi
+
+publish: release
+	$(V)./hack/release.sh --publish;
 
 #:vim set noexpandtab shiftwidth=8 softtabstop=0
--- hack/dependencies.sh
+++ hack/dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y curl apt-transport-https
+apt-get install -y curl apt-transport-https git socat golang-1.9
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial edge" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get -y install socat docker-ce
+apt-get -y install docker-ce
